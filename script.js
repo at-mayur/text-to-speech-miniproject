7e79c1204b1e6d8bf5f5c0fa9e97db3b965cc74f
Mayur <mayureshattarde@gmail.com>
2023-09-30 23:15:35 +0530

    added useful comments

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
+
+// creating SpeechSynthesisUtterance
 let speech = new SpeechSynthesisUtterance();
 
+
+// fetching all elements
 let textElem = document.getElementById("text-elem");
 let listenBtn = document.getElementById("listen-btn");
 let selectElem = document.getElementById("voice-selector");
 
 let voices = [];
 
+// On voice list load
 window.speechSynthesis.onvoiceschanged = () => {
     voices = window.speechSynthesis.getVoices();
+
+    // set default voice i.e. firts voice from list
     speech.voice = voices[0];
 
+    // update voice list
     voices.forEach((voice, index) => {
         selectElem.options[index] = new Option(voice.name, index);
     });
 
 };
 
+
+// when user selects another voice
 selectElem.onchange = () => {
     speech.voice = voices[selectElem.value];
 };
 
 
+// playing voice on btn click
 listenBtn.addEventListener("click", () => {
     window.speechSynthesis.cancel();
     speech.text = textElem.value;
